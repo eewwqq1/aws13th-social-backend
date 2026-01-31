3f86e82fdb0d9996f9e9db50f3db076ee04ac466
eewwqq1 <rlaehdrbs90@gmail.com>
2026-01-31 14:52:12 +0900

    week5 네이티브쿼리 마무리

--- routers/users.py
+++ routers/users.py
@@ -59,6 +59,7 @@ async def update_user(
     patch_dict = update_data.model_dump(exclude_unset=True, exclude_none=True,mode="json")
     return update_my_profile(db,current_user, patch_dict)
 
+
 @router.delete("/users/me",status_code=status.HTTP_200_OK)
 async def delete_user(
         current_user: Annotated[dict, Depends(get_current_user)],
